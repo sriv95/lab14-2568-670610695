3f3769984c08f86f1a6d5c3493562552407105bf
Parzival <srivichai95@gmail.com>
2025-09-15 20:11:24 +0700

    2. Password in Store.ts

--- src/libs/Store.ts
+++ src/libs/Store.ts
@@ -5,12 +5,14 @@ interface MarathonFormState {
   plan: "funrun" | "mini" | "half" | "full";
   gender: "male" | "female";
   email: string;
+  password: string;
   // Setters
   setFname: (v: string) => void;
   setLname: (v: string) => void;
   setPlan: (v: MarathonForm["plan"]) => void;
   setGender: (v: MarathonForm["gender"]) => void;
   setEmail: (v: string) => void;
+  setPassword: (v: string) => void;
   // Function ชื่อ discountCupon คำนวณ total ตรงนี้
   reset: () => void;
 }
